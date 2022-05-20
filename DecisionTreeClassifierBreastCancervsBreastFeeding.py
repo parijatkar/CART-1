6507dff0e6f5e71815be213741354dec0e8b2d11
Parijat Kar <parijatpkar@gmail.com>
2022-05-19 21:04:24 -0700

    Add files via upload

--- DecisionTreeClassifierBreastCancervsBreastFeeding.py
+++ DecisionTreeClassifierBreastCancervsBreastFeeding.py
@@ -13,8 +13,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
 from sklearn import tree
-#import graphviz
+import graphviz
 from sklearn.tree import export_text
+import pylab
 
 col = [ 'subjectid','age','height','weight','size1','size2','pcount',
        'bfcount', 'fhistory','positive']
@@ -31,12 +32,12 @@ df.info()
 sns.countplot(df['positive'])
 
 
-X = df.drop(['positive', 'subjectid', 'height', 'weight'],axis=1)
+X = df.drop(['positive', 'subjectid'], axis=1)
 y = df[['positive']]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3,random_state=42)
 
 
-clf_model = DecisionTreeClassifier(criterion="gini", random_state=42,max_depth=3, min_samples_leaf=5)   
+clf_model = DecisionTreeClassifier(criterion="gini", random_state=42,max_depth=4, min_samples_leaf=5)   
 clf_model.fit(X_train,y_train)
 
 DecisionTreeClassifier(max_depth=3, min_samples_leaf=5, random_state=42)
@@ -51,20 +52,28 @@ print('Accuracy:', Z)
 #print(CM)
 
 target = list(df['positive'].unique())
+map_target = np.asarray(target).astype(str)
+print(map_target)
+
+print(target)
 feature_names = list(X.columns)
 
 print(feature_names)
 
 
-#dot_data = tree.export_graphviz(clf_model,
-#                                out_file=None, 
-#                      feature_names=feature_names,  
-#                      class_names=target,  
-#                      filled=True, rounded=True,  
-#                      special_characters=True)  
-#graph = graphviz.Source(dot_data)  
+dot_data = tree.export_graphviz(clf_model,
+                                out_file=None, 
+                      feature_names=feature_names,  
+                      class_names=map_target,  
+                      filled=True, rounded=True,  
+                      special_characters=True)  
+graph = graphviz.Source(dot_data)  
 
-#graph
+#graph.view()
+
+filename = graph.render(filename='DecisionTreeGraph')
+
+pylab.savefig('filename.png')
 
 
 r = export_text(clf_model, feature_names=feature_names)
